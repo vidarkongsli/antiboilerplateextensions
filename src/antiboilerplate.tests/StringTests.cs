e49c6e04e461417d6812e2f1d9163c9c5a0be1dc
Vidar Kongsli <vidar@kongs.li>
2019-01-03 15:07:04 +0100

    Add tests for isnullorempty and isnullorwhitespace

--- src/antiboilerplate.tests/StringTests.cs
+++ src/antiboilerplate.tests/StringTests.cs
@@ -16,5 +16,24 @@ namespace Antiboilerplate.Tests
                 .FromUtf8Base64()
                 .Should().Be(testString);
         }
+
+        [Fact]
+        public void ShouldReportIsNullOrEmptyCorrectly()
+        {
+            const string nullString = null;
+            nullString.IsNullOrEmpty().Should().BeTrue("A null string should report true");
+            string.Empty.IsNullOrEmpty().Should().BeTrue("An empty string should report true");
+            "Not empty".IsNullOrEmpty().Should().BeFalse();
+        }
+        
+        [Fact]
+        public void ShouldReportIsNullOrWhitespaceCorrectly()
+        {
+            const string nullString = null;
+            nullString.IsNullOrWhitespace().Should().BeTrue("A null string should report true");
+            string.Empty.IsNullOrWhitespace().Should().BeTrue("An empty string should report true");
+            "\t".IsNullOrWhitespace().Should().BeTrue("A tab is whitespace");
+            "Not empty".IsNullOrWhitespace().Should().BeFalse();
+        }
     }
 }
